8d7b299e41130801afd015ae4b899ed916780244
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-29 00:09:10 +0300

    done

---
+++ src/component/main/main.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './main.scss';
+
+class Main extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+    handleChange = (e) => {
+      const url = e.target.value;
+      // this.setState = update the state + fire the render method
+      this.setState({ url });
+    };
+    handleClick = (e) => {
+      const url = this.state.url;
+      this.setState({ url });
+      // console.log('state',url);
+  
+    };
+  
+    methodClick = (e) => {
+      // console.log('state',this.state);
+      const method = e.target.value;
+      this.setState({ method });
+      console.log(method);
+    };
+    render() {
+      return (
+        <main className="main">
+          <div id="url">
+            <h3>URL : </h3>
+            <input type="text" onChange={this.handleChange} />
+            <button onClick={this.handleClick}>GO!</button>
+          </div>
+          <div id="rest" >
+            <button onClick={this.methodClick} value="GET">GET</button>
+            <button onClick={this.methodClick} value="POST">POST</button>
+            <button onClick={this.methodClick} value="PUT">PUT</button>
+            <button onClick={this.methodClick} value="DELETE">DELETE</button>
+          </div>
+          <div id="result">
+            <section>{this.state.method}</section>
+            <section>{this.state.url}</section>
+          </div>
+  
+        </main>
+      );
+    }
+  }
+  
+export default Main;
+  
