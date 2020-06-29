dc1b8e3a983e02b7c024bc96c6722898de897b95
Reham-Omar <runner@fv-az18.u5yzxyzrdlfetjlprjbrnbkcqh.cx.internal.cloudapp.net>
2020-06-29 23:44:58 +0000

    Updates

---
+++ precache-manifest.e8c012120a7960f3341a41b2bca1f283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7866925483b67c6e997ca089173220f1",
+    "url": "/rusty/index.html"
+  },
+  {
+    "revision": "35fca45b78ff2e490b4e",
+    "url": "/rusty/static/css/main.4398beff.chunk.css"
+  },
+  {
+    "revision": "02f303647c74a239c26f",
+    "url": "/rusty/static/js/2.4d906ac8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rusty/static/js/2.4d906ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35fca45b78ff2e490b4e",
+    "url": "/rusty/static/js/main.a4250900.chunk.js"
+  },
+  {
+    "revision": "9f6e4acaa7270368e021",
+    "url": "/rusty/static/js/runtime-main.e0a36da4.js"
+  }
+]);
