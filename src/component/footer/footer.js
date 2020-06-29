fb234ff957132c3f73e36d190d0b6c4fe8abe928
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-29 18:57:47 +0300

    edit

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './footer.scss';
-const Footer = () => {
-    return (
-      <footer>
-        &copy; 2020 Reham Omar ^_^
-      </footer>
-    );
-  };
 
+  class Footer extends React.Component {
+    render() {
+      return (
+        <footer>
+          &copy; 2020 Reham Omar ^_^
+        </footer>
+      );    }
+  }
+  
   export default Footer;
--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,52 +1,76 @@
 import React from 'react';
 import './main.scss';
 
-class Main extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
+class Form extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      method: '',
+      request: {},
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if ( this.state.url && this.state.method ) {
+
+      // Make an object that would be suitable for superagent
+      let request = {
+        url: this.state.url,
+        method: this.state.method,
+      };
+
+      // Clear old settings
+      let url = '';
+      let method = '';
+
+      this.setState({request, url, method});
+      e.target.reset();
+
     }
-    handleChange = (e) => {
-      const url = e.target.value;
-      // this.setState = update the state + fire the render method
-      this.setState({ url });
-    };
-    handleClick = (e) => {
-      const word = this.state.url;
-      this.setState({ word });
-      // console.log('state',url);
-  
-    };
-  
-    methodClick = (e) => {
-      // console.log('state',this.state);
-      const method = e.target.value;
-      this.setState({ method });
-      // console.log(method);
-    };
-    render() {
-      return (
-        <main className="main">
-          <div id="url">
-            <h3>URL : </h3>
-            <input type="text" onChange={this.handleChange} />
-            <button onClick={this.handleClick}>GO!</button>
-          </div>
-          <div id="rest" >
-            <button onClick={this.methodClick} value="GET">GET</button>
-            <button onClick={this.methodClick} value="POST">POST</button>
-            <button onClick={this.methodClick} value="PUT">PUT</button>
-            <button onClick={this.methodClick} value="DELETE">DELETE</button>
-          </div>
-          <div id="result">
-            <section>{this.state.method}</section>
-            <section>{this.state.word}</section>
-          </div>
-  
-        </main>
-      );
+
+    else {
+      alert('missing information');
     }
   }
-  
-export default Main;
+
+  handleChangeURL = e => {
+    const url = e.target.value;
+    this.setState({url});
+  };
+
+  handleChangeMethod = e => {
+    const method = e.target.id;
+    this.setState({ method });
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label >
+            <span>URL: </span>
+            <input name='url' type='text' onChange={this.handleChangeURL} />
+            <button type="submit">GO!</button>
+          </label>
+          <label className="methods">
+            <span className={this.state.method === 'get' ? 'active' : ''} id="get" onClick={this.handleChangeMethod}>GET</span>
+            <span className={this.state.method === 'post' ? 'active' : ''} id="post" onClick={this.handleChangeMethod}>POST</span>
+            <span className={this.state.method === 'put' ? 'active' : ''} id="put" onClick={this.handleChangeMethod}>PUT</span>
+            <span className={this.state.method === 'delete' ? 'active' : ''} id="delete" onClick={this.handleChangeMethod}>DELETE</span>
+          </label>
+        </form>
+        <section className="results">
+          <span className="method">{this.state.request.method}</span>
+          <span className="url">{this.state.request.url}</span>
+        </section>
+      </>
+    );
+  }
+}
+
+export default Form;
   
