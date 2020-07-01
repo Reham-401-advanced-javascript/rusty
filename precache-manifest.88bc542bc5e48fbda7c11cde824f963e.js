3f8117699408ab5af3b9cb21defafac2f56dd8f6
Reham-Omar <runner@fv-az71.3rdhylqnbmuu5npaw1dtkrqzeb.bx.internal.cloudapp.net>
2020-07-01 22:05:11 +0000

    Updates

--- precache-manifest.88bc542bc5e48fbda7c11cde824f963e.js
+++ precache-manifest.88bc542bc5e48fbda7c11cde824f963e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9053d43d976b8bfbe4773db087f4496c",
+    "revision": "de3e45b425bf8a9606e0f1c1ef2c69e1",
     "url": "/rusty/index.html"
   },
   {
-    "revision": "01c5e3489a42d8dc9d09",
+    "revision": "0f99eaa79b109c4ac00c",
     "url": "/rusty/static/css/main.4398beff.chunk.css"
   },
   {
-    "revision": "02f303647c74a239c26f",
-    "url": "/rusty/static/js/2.4d906ac8.chunk.js"
+    "revision": "18d9e13988852935c9b9",
+    "url": "/rusty/static/js/2.8cb5ba7e.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/rusty/static/js/2.4d906ac8.chunk.js.LICENSE.txt"
+    "url": "/rusty/static/js/2.8cb5ba7e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "01c5e3489a42d8dc9d09",
-    "url": "/rusty/static/js/main.1848c778.chunk.js"
+    "revision": "0f99eaa79b109c4ac00c",
+    "url": "/rusty/static/js/main.d891c718.chunk.js"
   },
   {
     "revision": "9f6e4acaa7270368e021",
